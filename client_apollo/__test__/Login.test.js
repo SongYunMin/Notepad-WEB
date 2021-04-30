30d5e87ac687d19f95fe6d62dc80e2ef5e3a0b93
Song-YunMin <thd930308@naver.com>
2021-05-01 01:30:53 +0900

    [UPDATE] Client Test

--- client_apollo/__test__/Login.test.js
+++ client_apollo/__test__/Login.test.js
@@ -1,33 +1,39 @@
 const {snapshot} = require('./screen');
 const puppeteer = require('puppeteer');
-
-describe("Login Test", () => {
-    beforeAll(async () => {
+describe("Test Start", () => {
+    beforeEach(async () => {
         await page.goto('http://localhost:8080', { waitUntil: 'domcontentloaded' });
-    });
-    test("Login Logout Test", async () => {
-        const browser = await puppeteer.launch({
-            headless: false
-        })
-        const page = await browser.newPage();
-        await page.goto('http://localhost:8080', {
-            waitUntil: "domcontentloaded"
-        });
-        const idInput = '.idInput';
-        const pwInput = '.pwInput';
-        const submit = '.signIn';
-
-        await page.type(idInput, "1234");
-        await page.type(pwInput, "1234");
-        await page.click(submit);
-
-        await snapshot('login-Test');
-        const login = await page.on("dialog", async (dialog) => {
-            await dialog.dismiss();
-            return dialog.message()
-        })
-        if(login === '로그인 되었습니다.'){
-            expect(true).toBe(true);
-        }
     })
+
+    test("Title Check", async () => {
+        const title = await page.title();
+        console.log("페이지 : ", page);
+        expect(title).toBe('client');
+    })
+
+    describe("User Test", () => {
+        beforeEach(async () => {
+            const idInput = '.idInput';
+            const pwInput = '.pwInput';
+            const submit = '.signIn';
+            console.log(idInput, pwInput);
+            await page.type(idInput, "1234");
+            await page.type(pwInput, "1234");
+            await page.click(submit);
+
+            await snapshot('login-Test');
+            await page.on("dialog", async (dialog) => {
+                await dialog.dismiss();
+                return dialog.message()
+            })
+        })
+
+        test("Logout Test", async () => {
+            // await page.click('.logout')
+            // await page.on('dialog', async (dialog) => {
+            //     console.log(await dialog.message());
+            // })
+        })
+    })
+
 })
